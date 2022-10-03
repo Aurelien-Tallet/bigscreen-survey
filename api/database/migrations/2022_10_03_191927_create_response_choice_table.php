8890fdf15a32bc1e76385fb4f9f4e464e33cb098
Aurelien-Tallet <aurelien.jarte@gmail.com>
2022-10-03 23:11:38 +0200

    migration and model added

---
+++ api/database/migrations/2022_10_03_191927_create_response_choice_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('response_choice', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignId('response_id')->constrained();
+            $table->foreignId('choice_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('response_choice');
+    }
+};
