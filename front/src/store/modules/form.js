c3dfa43ddb63f076ad1c2ba05972e6f308a5732d
subskill-anezzar <a.nezzar@subskill.com>
2022-10-06 11:46:59 +0200

    merge

--- front/src/store/modules/form.js
+++ front/src/store/modules/form.js
@@ -1,55 +1,47 @@
 const store = {
-    namespaced: true,
-    state: {
-        questionsComponents: {},
-        questionsResponses: [],
-// {
-//   "id": 1,
-//     "type": {
-//   "id": 1,
-//       "name": "textarea"
-// },
-//   "response": "test.test@gmail.com"
-// }
+  namespaced: true,
+  state: {
+    questionsComponents: {},
+    questionsResponses: [],
+  },
+  mutations: {
+    setQuestionComponent(state, value) {
+      state.questionsComponents[value.id] = value.valid;
     },
-    mutations: {
-        setQuestionComponent(state, value) {
-            state.questionsComponents[value.id] = value.valid;
-        },
-        setQuestionResponse(state, value) {
-            let found = false;
-            let index = 0;
-            state.questionsResponses.forEach((question,i) => {
-                if(question.id === value.id){
-                    found = true;
-                    index = i;
-                }
-            })
-            if(found){
-                state.questionsResponses[index] = value;
-            } else{
-                state.questionsResponses.push(value)
-            }
-        },
-    },
-    actions: {
-        setQuestionComponent({commit}, questionComponent) {
-            commit("setQuestionComponent", questionComponent);
-        },
-        setQuestionResponse({commit}, question) {
-            commit("setQuestionResponse", question)
+    setQuestionResponse(state, value) {
+      let found = false;
+      let index = 0;
+      state.questionsResponses.forEach((question, i) => {
+        if (question.id === value.id) {
+          found = true;
+          index = i;
         }
+      });
+      if (found) {
+        state.questionsResponses[index] = value;
+      } else {
+        state.questionsResponses.push(value);
+      }
     },
-    getters: {
-        questionsComponents: (state) => state.questionsComponents,
-        questionsResponses: (state) => state.questionsResponses,
-        isAllQuestionsValid: (state) => {
-            const questions = state.questionsComponents;
-            let validCount = 0;
-            for (let key in questions) questions[key] && validCount++;
-            return validCount === Object.keys(questions).length;
-        },
+  },
+  actions: {
+    setQuestionComponent({ commit }, questionComponent) {
+      commit("setQuestionComponent", questionComponent);
     },
+    setQuestionResponse({ commit }, question) {
+      commit("setQuestionResponse", question);
+    },
+  },
+  getters: {
+    questionsComponents: (state) => state.questionsComponents,
+    questionsResponses: (state) => state.questionsResponses,
+    isAllQuestionsValid: (state) => {
+      const questions = state.questionsComponents;
+      let validCount = 0;
+      for (let key in questions) questions[key] && validCount++;
+      return validCount === Object.keys(questions).length;
+    },
+  },
 };
 
 export default store;
