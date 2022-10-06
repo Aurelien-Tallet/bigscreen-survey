b4125a694dc2af2a6a3024a3f095fbbf16193a3d
subskill-anezzar <a.nezzar@subskill.com>
2022-10-06 15:08:57 +0200

    API Authentication routes and behaviour created

--- api/routes/api.php
+++ api/routes/api.php
@@ -28,5 +28,14 @@ Route::post('/forms/{id}', [FormController::class, 'submit']);
 Route::prefix('data')->group(function () {
     Route::get('question/{id}', [QuestionController::class, 'getResponses']);
     Route::get('submissions/{formId}', [FormController::class, 'getAllSubmissions']);
+});
+// Auth
+Route::prefix('auth')->group(function () {
+    Route::post('signup', 'App\Http\Controllers\Api\Auth\AuthController@signup')->name('auth.signup');
+    Route::post('login', 'App\Http\Controllers\Api\Auth\AuthController@login')->name('auth.login');
+    Route::post('logout', 'App\Http\Controllers\Api\Auth\AuthController@logout')->middleware('auth:sanctum')->name('auth.logout');
+    Route::get('user', 'App\Http\Controllers\Api\Auth\AuthController@getAuthenticatedUser')->middleware('auth:sanctum')->name('auth.user');
 
+    Route::post('/password/email', 'App\Http\Controllers\Api\Auth\AuthController@sendPasswordResetLinkEmail')->middleware('throttle:5,1')->name('password.email');
+    Route::post('/password/reset', 'App\Http\Controllers\Api\Auth\AuthController@resetPassword')->name('password.reset');
 });
