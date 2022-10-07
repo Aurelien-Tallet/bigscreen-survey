67953bfea08d0fef765f3df34c0a53b5d45f2c35
subskill-anezzar <a.nezzar@subskill.com>
2022-10-07 20:55:42 +0200

    Layout front style changed

--- front/src/views/Front/FormPage/FormPage.vue
+++ front/src/views/Front/FormPage/FormPage.vue
@@ -1,12 +1,14 @@
 <script>
 import Question from "@/components/Question/Question.vue";
-import { createNamespacedHelpers } from "vuex";
+import {createNamespacedHelpers} from "vuex";
+import FrontLayout from "@/views/Front/FrontLayout/FrontLayout.vue";
 
-const { mapActions, mapGetters } = createNamespacedHelpers("form");
+const {mapActions, mapGetters} = createNamespacedHelpers("form");
 
 export default {
   name: "form-page",
   components: {
+    FrontLayout,
     Question,
   },
   data() {
@@ -24,8 +26,8 @@ export default {
       if (this.$store.getters["form/isAllQuestionsValid"]) {
         try {
           const request = await this.$FormDataService.submit(
-            { questions: this.$store.getters["form/questionsResponses"] },
-            this.form.id
+              {questions: this.$store.getters["form/questionsResponses"]},
+              this.form.id
           );
           this.submission = request.submission;
         } catch (e) {
@@ -42,52 +44,52 @@ export default {
     this.form = await this.$FormDataService.get(1);
     this.questions = this.form.questions;
     this.questions.forEach((el) => {
-      this.setQuestionComponent({ id: el.id, valid: false });
+      this.setQuestionComponent({id: el.id, valid: false});
     });
   },
 };
 </script>
 
 <template>
-  <main class="form-page">
-    <form action="" class="questions-form" @submit.prevent="handleSubmit">
-      <ul class="questions-list">
-        <Question
-          v-for="(question, i) in questions"
-          :key="i"
-          :data="question"
-          :questionIndex="i"
-          :activeQuestion="activeQuestion"
-          :questionsLength="questions.length"
-          @incrementIndex="activeQuestion++"
-          @decrementIndex="activeQuestion--"
-        />
-      </ul>
+  <FrontLayout name="form-page">
+      <form action="" class="questions-form" @submit.prevent="handleSubmit">
+        <ul class="questions-list">
+          <Question
+              v-for="(question, i) in questions"
+              :key="i"
+              :data="question"
+              :questionIndex="i"
+              :activeQuestion="activeQuestion"
+              :questionsLength="questions.length"
+              @incrementIndex="activeQuestion++"
+              @decrementIndex="activeQuestion--"
+          />
+        </ul>
 
-      <button
-        :disabled="
+        <button
+            :disabled="
           !this.$store.getters['form/isAllQuestionsValid'] || isSubmitted
         "
-        class="cta"
-        type="submit"
-      >
-        Finaliser
-      </button>
-    </form>
+            class="cta"
+            type="submit"
+        >
+          Finaliser
+        </button>
+      </form>
 
-    <div class="form-message" v-if="isSubmitted">
-      <p v-if="this.submission.length">
-        Toute l’équipe de Bigscreen vous remercie pour votre engagement. Grâce à
-        votre investissement, nous vous préparons une application toujours plus
-        facile à utiliser, seul ou en famille.<br />
-        Si vous désirez consulter vos réponse ultérieurement, vous pouvez
-        consulter cette adresse:
-        <a :href="`${origin}/my-submission/${submission}`" target="_blank">{{
-          `${origin}/my-submission/${submission}`
-        }}</a>
-      </p>
-    </div>
-  </main>
+      <div class="form-message" v-if="isSubmitted">
+        <p v-if="this.submission.length">
+          Toute l’équipe de Bigscreen vous remercie pour votre engagement. Grâce à
+          votre investissement, nous vous préparons une application toujours plus
+          facile à utiliser, seul ou en famille.<br/>
+          Si vous désirez consulter vos réponse ultérieurement, vous pouvez
+          consulter cette adresse:
+          <a :href="`${origin}/my-submission/${submission}`" target="_blank">{{
+              `${origin}/my-submission/${submission}`
+            }}</a>
+        </p>
+      </div>
+  </FrontLayout>
 </template>
 
 <style lang="scss" scoped>
---
+++ front/src/views/Front/FrontLayout/FrontLayout.vue
@@ -0,0 +1,32 @@
+<script>
+import {RouterLink, RouterView} from "vue-router";
+
+export default {
+  name: "front-layout",
+  props: {
+    name: {},
+  },
+}
+</script>
+
+<template>
+  <div class="AppLayout">
+    <nav class="AppLayout__nav">
+      <ul class="nav-links">
+        <li class="nav-link">
+          <router-link to="/">Sondage</router-link>
+        </li>
+      </ul>
+    </nav>
+    <main :class="name" class="AppLayout__main">
+      <slot></slot>
+    </main>
+    <footer class="AppLayout__footer">
+      footer
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "FrontLayout.scss";
+</style>
