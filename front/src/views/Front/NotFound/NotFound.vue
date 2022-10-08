d8d0d96ff3cb55c2a08d62c3342383ac8f6a403e
subskill-anezzar <a.nezzar@subskill.com>
2022-10-08 19:30:25 +0200

    404 page added

---
+++ front/src/views/Front/NotFound/NotFound.vue
@@ -0,0 +1,23 @@
+<script>
+import FrontLayout from "@/views/Front/FrontLayout/FrontLayout.vue";
+
+export default {
+  name: "not-found",
+  components: {
+    FrontLayout,
+  },
+};
+</script>
+
+<template>
+  <FrontLayout name="form-page">
+    <div class="not-found-content">
+    <h1>Oops ! La page que vous cherchez n'existe pas</h1>
+    <img class="helmet-vr" src="@/assets/images/vr.svg" alt="logo de BigScreen"/>
+    </div>
+  </FrontLayout>
+</template>
+
+<style lang="scss" scoped>
+@import "NotFound";
+</style>
