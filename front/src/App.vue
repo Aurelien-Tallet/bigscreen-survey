f632b6577c32b043bdead40149f2f009968150e7
subskill-anezzar <a.nezzar@subskill.com>
2022-10-09 19:30:36 +0200

    Conflicts fixed

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,22 +2,22 @@
 import ApiDataService from "@/services/ApiDataService";
 
 export default {
-  //PING API
   async created() {
+    //PING API to Check if it is online
     try {
       const req = await ApiDataService.ping();
       if (req.status === 200) {
         this.$store.commit("SET_API_STATUS", true);
       }
     } catch (e) {
-      console.error("API NOT FOUND");
+      console.error("API not found or offline");
     }
   },
 };
 </script>
 
 <template>
-  <RouterView />
+  <RouterView/>
 </template>
 
 <style scoped></style>
